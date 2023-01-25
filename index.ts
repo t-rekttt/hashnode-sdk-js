4684d443c3b97b180d0ea8e9e1bcefdc6ee59603
T-Rekt <t.to.the.rekt@gmail.com>
2023-01-25 18:52:59 +0700

    Added missing exports to index.ts

--- index.ts
+++ index.ts
@@ -4,3 +4,5 @@ export { default as createPublicationArticle } from "./src/create-publication-ar
 export { default as updatePublicationArticle } from "./src/update-publication-article";
 export { default as uploadImage } from "./src/upload-image";
 export { default as updatePublicationArticleUnofficial } from "./src/update-publication-article-unofficial";
+export { default as createDraftUnofficial } from "./src/create-draft-unoffical";
+export { default as updateDraftUnofficial } from "./src/update-draft-unofficial";
